23b5dc4da005ddc89556c778656e2d478dbf9e28
Krishan <kslg257@gmail.com>
2022-08-10 06:11:47 +0000

    Add template, migration and models

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+# Inherit all the base functionality from the built-in Django model class.
+# The null equals false attribute here prevents items from being created without a name programmatically.
+# Blank equals false will make the field required on forms.
+# Default value of false here just to make sure that to-do items are marked as not done by default.
+class Item(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    done = models.BooleanField(null=False, blank=False , default=False)
+
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
-def say_hello(request):
-    return HttpResponse("Hello!")
+def get_todo_list(request):
+    return render(request, 'todo/todo_list.html')
