1fbb86e9342f566ffe1b5998c253c35e20fbd1de
Krishan <kslg257@gmail.com>
2022-08-11 19:50:21 +0000

    Update models

--- todo/models.py
+++ todo/models.py
@@ -10,3 +10,6 @@ class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False , default=False)
 
+    def __str__(self):
+       return self.name 
+
