07f2b077cc65459d0619fbc7395ebe9bcf0cc4c6
Krishan <kslg257@gmail.com>
2022-08-11 20:49:01 +0000

    Rendering Data

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,4 @@
+"""Imports"""
 from django.db import models
 
 # Create your models here.
@@ -6,10 +7,11 @@ from django.db import models
 # The null equals false attribute here prevents items from being created without a name programmatically.
 # Blank equals false will make the field required on forms.
 # Default value of false here just to make sure that to-do items are marked as not done by default.
+
+
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
-    done = models.BooleanField(null=False, blank=False , default=False)
+    done = models.BooleanField(null=False, blank=False, default=False)
 
     def __str__(self):
-       return self.name 
-
+        return self.name
