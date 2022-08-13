ced887aef2e77540aea641f48a0b089544cf6180
Krishan <kslg257@gmail.com>
2022-08-13 08:49:16 +0000

    READ AND CREATE CRUD

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -13,11 +13,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+"""
+Import the new html templates names into todo.views to make it accessible to urls.py
+"""
 from django.contrib import admin
 from django.urls import path
-from todo.views import get_todo_list
+from todo.views import get_todo_list, add_item
 
+"""
+path (URL, VIEW, NAME)
+"""
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', get_todo_list, name='get_todo_list')
+    path('', get_todo_list, name='get_todo_list'),
+    path('add', add_item, name='add')
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
 # Create your views here.
 
@@ -9,3 +9,13 @@ def get_todo_list(request):
         'items': items
     }
     return render(request, 'todo/todo_list.html', context)
+
+
+def add_item(request):
+    if request.method == 'POST':
+        name = request.POST.get('item_name')
+        done = 'done' in request.POST
+        Item.ojects.create(name=name, done=done)
+
+        return redirect('get_todo_list')
+    return render(request, 'todo/add_item.html')
