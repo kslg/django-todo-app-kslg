f447fbe6d46cc76904169cb61b70659ec52a6038
Krishan <kslg257@gmail.com>
2022-08-15 20:47:37 +0000

    Updates settings.py database settings and ran migrations.

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,11 +75,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://aruanioibyvxiq:841402614ed969e15ca62a71c23c76381292d649f6a3353889fb09a25960b0db@ec2-52-49-120-150.eu-west-1.compute.amazonaws.com:5432/d4rc8ms9vemgfr')
 }
 
 
